344f4e03debedf2a9268434447acd0c5ae18e81b
jordiadan43 <jordiadan43@gmail.com>
2018-05-15 10:41:50 +0200

    slider final

--- slider1.js
+++ slider1.js
@@ -1,31 +1,44 @@
-var slider = document.getElementById('slider');
-var sliderImages = document.getElementsByClassName('slider-img')
-
+var slider = document.getElementById('imagenes');
+var sliderImages = slider.getElementsByClassName('slider-img');
 var nextButton = document.getElementById('next-btn');
-var previousButton
+var previousButton = document.getElementById('previous-btn');
 
+nextButton.addEventListener("click", next);
+previousButton.addEventListener("click", previous);
+
+// Ocultar todas las imágenes
 [].forEach.call(sliderImages, function (elementImage) {
   hideImage(elementImage);
-  }
-
 });
 
-var index = 0;
-//muestra la primera
+var index = 0; // First image to be shown
+var nImages = sliderImages.length;
+
+// Muestra la primera imagen (elemento con index 0)
 showImage(sliderImages[index]);
 
+// Hide an image
+function hideImage(elementImage) { elementImage.style.display = "none"; }
 
-//hide an image
-function hideImage(elementImage) {
-  elementImage.style.display = "none";
-}
+// Show an image
+function showImage(elementImage) { elementImage.style.display = "block"; }
 
-//show an image
-function showImage(elementImage) {
-  elementImage.style.dysplay = "block"
-}
-
-//next image
+// Next image
 function next() {
-  index = (index + 1) % nImage
+  hideImage(sliderImages[index]);
+  index = (index + 1) % nImages;
+  showImage(sliderImages[index]);
+}
+
+// Previous image
+function previous() {
+  hideImage(sliderImages[index]);
+
+  if (index == 0) {
+    index = nImages - 1;
+  } else {
+    index = (index - 1);
+  }
+
+  showImage(sliderImages[index]);
 }
